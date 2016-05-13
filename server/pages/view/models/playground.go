6ff7b607eb809c8fb5661b7c85c9f99cd58e9e2d
peterhoward42 <peterhoward42@gmail.com>
2016-05-13 22:45:37 +0100

    wip - Refactoring html so that the input panel and the output panel can be shown either side by side, or in a tabbed view.

--- server/pages/view/models/playground.go
+++ server/pages/view/models/playground.go
@@ -3,13 +3,26 @@ package models
 // The Playground struct provides state information for rendering the playground view.
 // For example, which is the currently selected page.
 type Playground struct {
-	InputText  string
-	OutputText string
+	SideBySidePanels         bool
+	TabbedPanels             bool
+	ShowInputTab             bool
+	ShowFrostTab             bool
+	TabsOrSideBySideLinkText string
+	InputText                string
+	OutputText               string
 }
 
 // The NewPlayground function is a factory that makes Playground view models.
 func NewPlayground(inputTxt string,
 	outputTxt string) *Playground {
-	model := &Playground{inputTxt, outputTxt}
+	model := &Playground{
+		SideBySidePanels:         false,
+		TabbedPanels:             true,
+		ShowInputTab:             false,
+		ShowFrostTab:             true,
+		TabsOrSideBySideLinkText: "Tabbed view",
+		InputText:                inputTxt,
+		OutputText:               outputTxt,
+	}
 	return model
 }
