b12d3572a2f1106218c539c98becd225a90868c2
peterhoward42 <peterhoward42@gmail.com>
2016-06-26 15:30:51 +0100

    Added new entry point variant for AWS Beanstalk

--- entrypoints/aws/beanstalk/main.go
+++ entrypoints/aws/beanstalk/main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"github.com/peterhoward42/frost/server/routing"
 	"net/http"
+	"os"
 )
 
 func main() {
 	routing.SetUpRouting()
-	port := ":5000"
+
+	// We must respect the port allocated by the hosting environment, on AWS Beanstalk
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	port = ":" + port
+	
 	fmt.Printf("Serving on port %v", port)
 	http.ListenAndServe(port, nil)
 }
